6cebccec8c864277ecf062a3135e21d722ccb000
xutao <1844325217@qq.com>
2019-03-31 16:49:38 +0800

    后台管理 - 同意退款 - 支付宝退款

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Order extends Model
 {
@@ -100,5 +101,15 @@ class Order extends Model
         \Log::warning('find order no failed');
 
         return false;
+    }
+
+    public static function getAvailableRefundNo()
+    {
+        do {
+            // Uuid 类可以用来生成大概率不重复的字符串
+            $no = Uuid::uuid4()->getHex();
+        } while (static::query()->where('refund_no', $no)->exists());
+
+        return $no;
     }
 }
